b60dccf2b492bcd33a6e74b1c39c36814ef7c680
sakhter <sakhter@270b.com>
2019-11-15 16:21:23 -0500

    converted the news feed form to create post to react component

---
+++ src/components/Common/PostControlButton.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function PostControlButton() {
+    return (
+        <div className="control-block-button post-control-button">
+
+            <a href="#" className="btn btn-control likes">
+                <img src="img/heart-icon.png" alt="" />
+                <div className="ripple-container"></div>
+            </a>
+
+            <a href="#" className="btn btn-control ratings">
+                <img src="img/star-icon2.png" alt="" />
+            </a>
+
+            <a href="#" className="btn btn-control share-link">
+                <img src="img/share-icon.png" alt="" />
+            </a>
+            <a href="#" className="btn btn-control comments">
+                <img src="img/comments-icon.png" alt="" />
+            </a>
+
+        </div>
+    )
+}
