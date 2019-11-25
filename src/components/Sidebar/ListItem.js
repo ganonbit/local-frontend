434b6059080373057c553be91727a9ad05171808
sakhter <sakhter@270b.com>
2019-11-25 10:56:06 -0500

    Added dynamic Tooltips in Sidebar and createPost component/Fixed displaying issues

---
+++ src/components/Sidebar/ListItem.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+const ListItem = ({
+  linkClass,
+  titleClass,
+  title,
+  onSetSidebarOpen,
+  sideBarOpenModel
+}) => (
+  <li>
+    <NavLink className={linkClass} to="/home">
+      <svg
+        className="olymp-close-icon left-menu-icon"
+        data-toggle="tooltip"
+        data-placement="right"
+        data-original-title="FRIEND GROUPS">
+        {/* <use xlink:href="svg-icons/sprites/icons.svg#olymp-happy-faces-icon"></use> */}
+      </svg>
+      <span
+        className={titleClass}
+        onClick={() =>
+          sideBarOpenModel && onSetSidebarOpen(false, 'fixed-sidebar')
+        }>
+        {title}
+      </span>
+    </NavLink>
+  </li>
+)
+export default ListItem
