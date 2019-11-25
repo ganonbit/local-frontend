214c6e4c65103114745ca79874c078df22883d34
sakhter <sakhter@270b.com>
2019-11-25 10:52:12 -0500

    implemented OverlayTrigger ToolTip Component using React-bootstrap

---
+++ src/components/Common/ToolTip.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Tooltip } from 'react-bootstrap'
+import { OverlayTrigger } from 'react-bootstrap'
+
+const ToolTip = props => <Tooltip>{props}</Tooltip>
+
+const OverlayTriggers = props => {
+  return (
+    <OverlayTrigger
+      trigger="hover"
+      placement={props.placement}
+      delay={{ show: 0, hide: 1 }}
+      overlay={ToolTip(props.toolTipText)}>
+      {props.children}
+    </OverlayTrigger>
+  )
+}
+
+export default OverlayTriggers
