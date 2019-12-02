fbd38651a20154bdc0a58a883b4e06590c8f4ddf
sakhter <sakhter@270b.com>
2019-12-02 16:17:50 -0500

    resolved path broken issues for widget and comments

---
+++ src/components/Comments/AddComment.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { CREATE_COMMENT } from '../../graphql/comment';
+import { GET_FOLLOWED_POSTS } from '../../graphql/post';
+import { Mutation } from 'react-apollo';
+
+export default function AddComment({ authorId, postId, onCancel }) {
+	const [commentContent, setCommentContent] = useState({
+		comment: '',
+		image: '',
+		// imagePublicid: '',
+		author: authorId,
+		postId: postId,
+	});
+	const onAddComment = (e, createComment) => {
+		e.preventDefault();
+		createComment.then(async ({ data }) => {},
+		setCommentContent({ ...commentContent, comment: '', image: '' }));
+	};
+	const onCommentChange = e => {
+		e.preventDefault();
+		const { name, value } = e.target;
+		setCommentContent({ ...commentContent, [name]: value });
+	};
+	return (
+		<Mutation
+			mutation={CREATE_COMMENT}
+			variables={{
+				input: commentContent,
+			}}
+			refetchQueries={() => [
+				{
+					query: GET_FOLLOWED_POSTS,
+					variables: { userId: authorId, skip: 0, limit: 15 },
+				},
+			]}>
+			{(createComment, { loading, error: apiError }) => {
+				return (
+					<form
+						className='comment-form inline-items'
+						onSubmit={e => onAddComment(e, createComment())}>
+						<div className='post__author author vcard inline-items'>
+							<img src='img/author-page.jpg' alt='author' />
+
+							<div className='form-group with-icon-right '>
+								<textarea
+									type='text'
+									className='form-control'
+									placeholder=''
+									name='comment'
+									value={commentContent.comment}
+									onChange={e =>
+										onCommentChange(e)
+									}></textarea>
+								<div className='add-options-message'>
+									<a
+										href='#1'
+										className='options-message'
+										data-toggle='modal'
+										data-target='#update-header-photo'>
+										{/* <svg className="olymp-camera-icon">
+                                    <use xlink: href="svg-icons/sprites/icons.svg#olymp-camera-icon"></use>
+											</svg> */}
+									</a>
+								</div>
+							</div>
+						</div>
+
+						<button
+							className='btn btn-md-2 btn-primary'
+							type='post'>
+							Post Comment
+						</button>
+
+						<button
+							className='btn btn-md-2 btn-border-think c-grey btn-transparent custom-color'
+							onClick={() => onCancel(false)}>
+							Cancel
+						</button>
+					</form>
+				);
+			}}
+		</Mutation>
+	);
+}
+AddComment.propTypes = {
+	authorId: PropTypes.string.isRequired,
+	postId: PropTypes.string.isRequired,
+};
---
+++ src/components/widget/ActivityFeed.js
@@ -0,0 +1,313 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons';
+export default function ActivityFeed() {
+	return (
+		<div className='ui-block'>
+			<div className='ui-block-title block-title-bg'>
+				<h6 className='title'>Activity Feed</h6>
+				<a href='#1' className='more'>
+					<FontAwesomeIcon icon={faEllipsisV} color='white' />
+				</a>
+			</div>
+
+			<ul className='widget w-activity-feed notification-list'>
+				<li>
+					<div className='author-thumb'>
+						<img src='img/avatar49-sm.jpg' alt='author' />
+					</div>
+					<div className='notification-event'>
+						<a href='#1' className='h6 notification-friend'>
+							Marina Polson
+						</a>{' '}
+						commented on Jason Mark’s
+						<a href='#1' className='notification-link'>
+							photo.
+						</a>
+						.
+						<span className='notification-date'>
+							<time
+								className='entry-date updated'
+								dateTime='2004-07-24T18:18'>
+								2 mins ago
+							</time>
+						</span>
+					</div>
+				</li>
+
+				<li>
+					<div className='author-thumb'>
+						<img src='img/avatar9-sm.jpg' alt='author' />
+					</div>
+					<div className='notification-event'>
+						<a href='#1' className='h6 notification-friend'>
+							Jake Parker{' '}
+						</a>{' '}
+						liked Nicholas Grissom’s{' '}
+						<a href='#1' className='notification-link'>
+							status update.
+						</a>
+						.
+						<span className='notification-date'>
+							<time
+								className='entry-date updated'
+								dateTime='2004-07-24T18:18'>
+								5 mins ago
+							</time>
+						</span>
+					</div>
+				</li>
+
+				<li>
+					<div className='author-thumb'>
+						<img src='img/avatar50-sm.jpg' alt='author' />
+					</div>
+					<div className='notification-event'>
+						<a href='#1' className='h6 notification-friend'>
+							Mary Jane Stark{' '}
+						</a>{' '}
+						added 20 new photos to her{' '}
+						<a href='#1' className='notification-link'>
+							gallery album.
+						</a>
+						.
+						<span className='notification-date'>
+							<time
+								className='entry-date updated'
+								dateTime='2004-07-24T18:18'>
+								12 mins ago
+							</time>
+						</span>
+					</div>
+				</li>
+
+				<li>
+					<div className='author-thumb'>
+						<img src='img/avatar51-sm.jpg' alt='author' />
+					</div>
+					<div className='notification-event'>
+						<a href='#1' className='h6 notification-friend'>
+							Nicholas Grissom{' '}
+						</a>{' '}
+						updated his profile{' '}
+						<a href='#1' className='notification-link'>
+							photo
+						</a>
+						.
+						<span className='notification-date'>
+							<time
+								className='entry-date updated'
+								dateTime='2004-07-24T18:18'>
+								1 hour ago
+							</time>
+						</span>
+					</div>
+				</li>
+				<li>
+					<div className='author-thumb'>
+						<img src='img/avatar48-sm.jpg' alt='author' />
+					</div>
+					<div className='notification-event'>
+						<a href='#1' className='h6 notification-friend'>
+							Marina Valentine{' '}
+						</a>{' '}
+						commented on Chris Greyson’s{' '}
+						<a href='#1' className='notification-link'>
+							status update
+						</a>
+						.
+						<span className='notification-date'>
+							<time
+								className='entry-date updated'
+								dateTime='2004-07-24T18:18'>
+								1 hour ago
+							</time>
+						</span>
+					</div>
+				</li>
+
+				<li>
+					<div className='author-thumb'>
+						<img src='img/avatar52-sm.jpg' alt='author' />
+					</div>
+					<div className='notification-event'>
+						<a href='#1' className='h6 notification-friend'>
+							Green Goo Rock{' '}
+						</a>{' '}
+						posted a{' '}
+						<a href='#1' className='notification-link'>
+							status update
+						</a>
+						.
+						<span className='notification-date'>
+							<time
+								className='entry-date updated'
+								dateTime='2004-07-24T18:18'>
+								1 hour ago
+							</time>
+						</span>
+					</div>
+				</li>
+				<li>
+					<div className='author-thumb'>
+						<img src='img/avatar10-sm.jpg' alt='author' />
+					</div>
+					<div className='notification-event'>
+						<a href='#1' className='h6 notification-friend'>
+							Elaine Dreyfuss{' '}
+						</a>{' '}
+						liked your{' '}
+						<a href='#1' className='notification-link'>
+							blog post
+						</a>
+						.
+						<span className='notification-date'>
+							<time
+								className='entry-date updated'
+								dateTime='2004-07-24T18:18'>
+								2 hours ago
+							</time>
+						</span>
+					</div>
+				</li>
+
+				<li>
+					<div className='author-thumb'>
+						<img src='img/avatar10-sm.jpg' alt='author' />
+					</div>
+					<div className='notification-event'>
+						<a href='#1' className='h6 notification-friend'>
+							Elaine Dreyfuss{' '}
+						</a>{' '}
+						commented on your{' '}
+						<a href='#1' className='notification-link'>
+							blog post
+						</a>
+						.
+						<span className='notification-date'>
+							<time
+								className='entry-date updated'
+								dateTime='2004-07-24T18:18'>
+								2 hours ago
+							</time>
+						</span>
+					</div>
+				</li>
+
+				<li>
+					<div className='author-thumb'>
+						<img src='img/avatar53-sm.jpg' alt='author' />
+					</div>
+					<div className='notification-event'>
+						<a href='#1' className='h6 notification-friend'>
+							Bruce Peterson{' '}
+						</a>{' '}
+						changed his{' '}
+						<a href='#1' className='notification-link'>
+							profile picture
+						</a>
+						.
+						<span className='notification-date'>
+							<time
+								className='entry-date updated'
+								dateTime='2004-07-24T18:18'>
+								15 hours ago
+							</time>
+						</span>
+					</div>
+				</li>
+
+				<li>
+					<div className='author-thumb'>
+						<img src='img/avatar52-sm.jpg' alt='author' />
+					</div>
+					<div className='notification-event'>
+						<a href='#1' className='h6 notification-friend'>
+							Green Goo Rock{' '}
+						</a>{' '}
+						posted a{' '}
+						<a href='#1' className='notification-link'>
+							status update
+						</a>
+						.
+						<span className='notification-date'>
+							<time
+								className='entry-date updated'
+								dateTime='2004-07-24T18:18'>
+								1 hour ago
+							</time>
+						</span>
+					</div>
+				</li>
+				<li>
+					<div className='author-thumb'>
+						<img src='img/avatar10-sm.jpg' alt='author' />
+					</div>
+					<div className='notification-event'>
+						<a href='#1' className='h6 notification-friend'>
+							Elaine Dreyfuss{' '}
+						</a>{' '}
+						liked your{' '}
+						<a href='#1' className='notification-link'>
+							blog post
+						</a>
+						.
+						<span className='notification-date'>
+							<time
+								className='entry-date updated'
+								dateTime='2004-07-24T18:18'>
+								2 hours ago
+							</time>
+						</span>
+					</div>
+				</li>
+
+				<li>
+					<div className='author-thumb'>
+						<img src='img/avatar10-sm.jpg' alt='author' />
+					</div>
+					<div className='notification-event'>
+						<a href='#1' className='h6 notification-friend'>
+							Elaine Dreyfuss{' '}
+						</a>{' '}
+						commented on your{' '}
+						<a href='#1' className='notification-link'>
+							blog post
+						</a>
+						.
+						<span className='notification-date'>
+							<time
+								className='entry-date updated'
+								dateTime='2004-07-24T18:18'>
+								2 hours ago
+							</time>
+						</span>
+					</div>
+				</li>
+
+				<li>
+					<div className='author-thumb'>
+						<img src='img/avatar53-sm.jpg' alt='author' />
+					</div>
+					<div className='notification-event'>
+						<a href='#1' className='h6 notification-friend'>
+							Bruce Peterson{' '}
+						</a>{' '}
+						changed his{' '}
+						<a href='#1' className='notification-link'>
+							profile picture
+						</a>
+						.
+						<span className='notification-date'>
+							<time
+								className='entry-date updated'
+								dateTime='2004-07-24T18:18'>
+								15 hours ago
+							</time>
+						</span>
+					</div>
+				</li>
+			</ul>
+		</div>
+	);
+}
