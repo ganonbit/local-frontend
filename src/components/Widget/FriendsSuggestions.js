92664b56e8f88aa3cc1e4ba00e24542a73d9b29a
sakhter <sakhter@270b.com>
2019-12-03 17:09:32 -0500

    cleaned Up unused html/connect to CreateFollow apis

--- src/components/Widget/FriendsSuggestions.js
+++ src/components/Widget/FriendsSuggestions.js
@@ -1,77 +1,93 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Avatar from 'components/Avatar';
+import { Query } from 'react-apollo';
+import { withApollo } from 'react-apollo';
+import { generatePath, Link } from 'react-router-dom';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSmile, faEllipsisV } from '@fortawesome/free-solid-svg-icons';
-export default function FriendsSuggestions() {
+import { faEllipsisV, faUserPlus } from '@fortawesome/free-solid-svg-icons';
+
+import { USER_SUGGESTIONS } from 'graphql/user';
+import { CREATE_FOLLOW } from 'graphql/follow';
+
+import * as Routes from 'routes';
+
+import { useStore } from 'store';
+
+const FriendsSuggestions = ({ client }) => {
+  const [{ auth }] = useStore();
+  const variables = {
+    userId: auth.user.id,
+  };
+
+  let handleButtonClick = async (e, follower) => {
+    e.preventDefault();
+    try {
+      await client.mutate({
+        mutation: CREATE_FOLLOW,
+        variables: { input: { userId: auth.user.id, followerId: follower.id } },
+        refetchQueries: () => [
+          {
+            query: USER_SUGGESTIONS,
+            variables: variables,
+          },
+        ],
+      });
+    } catch (err) {}
+  };
   return (
-    <div className='ui-block'>
-      <div className='ui-block-title block-title-bg'>
-        <h6 className='title'>Friend Suggestions</h6>
-        <a href='#1' className='more'>
-          <FontAwesomeIcon icon={faEllipsisV} color='white' />
-        </a>
-      </div>
-
-      <ul className='widget w-friend-pages-added notification-list friend-requests'>
-        <li className='inline-items'>
-          <div className='author-thumb'>
-            <img src='img/avatar38-sm.jpg' alt='author' />
+    <Query query={USER_SUGGESTIONS} variables={variables}>
+      {({ data, loading }) => {
+        return loading === true ? (
+          <h1>loading...!</h1>
+        ) : (
+          <div className='ui-block'>
+            <div className='ui-block-title block-title-bg'>
+              <h6 className='title'>Friend Suggestions</h6>
+              <a href='#1' className='more'>
+                <FontAwesomeIcon icon={faEllipsisV} color='white' />
+              </a>
+            </div>
+            <ul className='widget w-friend-pages-added notification-list friend-requests sidebar-box'>
+              {data.suggestPeople.map((user, index) => (
+                <li className='inline-items' key={index}>
+                  <div className='author-thumb'>
+                    <Avatar image={user.image} />
+                  </div>
+                  <div className='notification-event'>
+                    <Link
+                      className='h6 notification-friend'
+                      to={generatePath(Routes.USER_PROFILE, {
+                        username: user.username,
+                      })}
+                    >
+                      {`${user.firstName} ${user.lastName}`}
+                    </Link>
+                    <span className='chat-message-item'>
+                      6 Friends in Common
+                    </span>
+                  </div>
+                  <span className='notification-icon'>
+                    <div
+                      className='accept-request'
+                      onClick={e => handleButtonClick(e, user)}
+                    >
+                      <span className='without-text'>
+                        <FontAwesomeIcon icon={faUserPlus} color='white' />
+                      </span>
+                    </div>
+                  </span>
+                </li>
+              ))}
+            </ul>
           </div>
-          <div className='notification-event'>
-            <a href='#1' className='h6 notification-friend'>
-              Francine Smith
-            </a>
-            <span className='chat-message-item'>8 Friends in Common</span>
-          </div>
-          <span className='notification-icon'>
-            <a href='#1' className='accept-request'>
-              <span className='icon-add without-text'>
-                <FontAwesomeIcon icon={faSmile} color='white' />
-                {/* <svg className="olymp-happy-face-icon">
-                                    <use xlink: href="svg-icons/sprites/icons.svg#olymp-happy-face-icon"></use>
-										</svg> */}
-              </span>
-            </a>
-          </span>
-        </li>
-
-        <li className='inline-items'>
-          <div className='author-thumb'>
-            <img src='img/avatar39-sm.jpg' alt='author' />
-          </div>
-          <div className='notification-event'>
-            <a href='#1' className='h6 notification-friend'>
-              Hugh Wilson
-            </a>
-            <span className='chat-message-item'>6 Friends in Common</span>
-          </div>
-          <span className='notification-icon'>
-            <a href='#1' className='accept-request'>
-              <span className='icon-add without-text'>
-                <FontAwesomeIcon icon={faSmile} color='white' />
-              </span>
-            </a>
-          </span>
-        </li>
-
-        <li className='inline-items'>
-          <div className='author-thumb'>
-            <img src='img/avatar40-sm.jpg' alt='author' />
-          </div>
-          <div className='notification-event'>
-            <a href='#1' className='h6 notification-friend'>
-              Karen Masters
-            </a>
-            <span className='chat-message-item'>6 Friends in Common</span>
-          </div>
-          <span className='notification-icon'>
-            <a href='#1' className='accept-request'>
-              <span className='icon-add without-text'>
-                <FontAwesomeIcon icon={faSmile} color='white' />
-              </span>
-            </a>
-          </span>
-        </li>
-      </ul>
-    </div>
+        );
+      }}
+    </Query>
   );
-}
+};
+FriendsSuggestions.propTypes = {
+  client: PropTypes.object.isRequired,
+};
+export default withApollo(FriendsSuggestions);
