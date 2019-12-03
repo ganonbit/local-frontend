a3f5f1fa08d595bebacb72b9a82f2bf69a65817b
Andrew Reese <ask@hyliancoder.com>
2019-12-02 18:05:31 -0600

    fixing logic

--- src/pages/Auth/VerifyAccount.js
+++ src/pages/Auth/VerifyAccount.js
@@ -40,84 +40,85 @@ const VerifyAccount = ({ history, location, refetch }) => {
 	const token = url.get('token');
 
 	return (
-		<div>
-			<h1>Verify Account</h1>
-
+		<div className='col col-xl-5 col-lg-6 col-md-12 col-sm-12 col-12'>
 			<div>
 				<Query query={VERIFY_TOKEN} variables={{ email, token }}>
 					{({ error: apiError }) => {
-						// if (apiError) {
-						// 	return (<h1>This token is either invalid or expired!</h1>);
-						// }
+						if (apiError) {
+							return (<h1 className='btn-green'>This token is either invalid or expired!</h1>);
+						}
 						return (
 							<Mutation
 								mutation={VERIFY_ACCOUNT}
-								variables={{ input: { email, token } }}>
+								variables={{ input: { email, token } }}
+							>
 								{(
 									verifyAccount,
 									{ loading, error: apiError }
 								) => {
-									if (apiError) return <h1>{apiError}</h1>;
+									if (apiError) return <h1 className='btn-green'>{apiError}</h1>;
 
 									return (
 										// <>
-											<div className='col col-xl-5 col-lg-6 col-md-12 col-sm-12 col-12'>
-												<div className='registration-login-form'>
-													<div className='tab-content'>
-														<div
-															className='tab-pane active'
-															id='home'
-															role='tabpanel'
-															data-mh='log-tab'>
-															<div className='title h6'>
-																Verify Account
-															</div>
-
-															<form	
-																className='content'
-																onSubmit={e =>
-																	handleSubmit(
-																		e,
-																		verifyAccount
-																	)
-																}>
-																<div className='row'>
-																<div className='col col-12 col-xl-12 col-lg-12 col-md-12 col-sm-12'></div>
-																	<Field
-																		type='text'
-																		name='token'
-																		values={
-																			token
-																		}
-																		placeholder={
-																			token
-																		}
-																		disabled="disabled"
-																	/>
-
-																	{error && (
-																		<div
-																			bottom='sm'
-																			top='sm'>
-																			<p>
-																				{
-																					error
-																				}
-																			</p>
-																		</div>
-																	)}
-
-																	<button
-																		className='btn btn-lg btn-primary full-width'
-																		>
-																		Verify Account
-																	</button>
-																</div>
-															</form>
+										<div>
+											<div className='registration-login-form'>
+												<div className='tab-content'>
+													<div
+														className='tab-pane active'
+														id='home'
+														role='tabpanel'
+														data-mh='log-tab'
+													>
+														<div className='title h6'>
+															Verify Account
 														</div>
+
+														<form
+															className='content'
+															onSubmit={e =>
+																handleSubmit(
+																	e,
+																	verifyAccount
+																)
+															}
+														>
+															<div className='row'>
+																<div className='col col-12 col-xl-12 col-lg-12 col-md-12 col-sm-12'></div>
+																<input
+																	type='text'
+																	name='token'
+																	values={
+																		token
+																	}
+																	placeholder={
+																		token
+																	}
+																	disabled="true"
+																/>
+
+																{error && (
+																	<div
+																		bottom='sm'
+																		top='sm'
+																	>
+																		<p>
+																			{
+																				error
+																			}
+																		</p>
+																	</div>
+																)}
+
+																<button className='btn btn-lg btn-primary full-width'>
+																	Verify
+																	Account
+																</button>
+															</div>
+														</form>
 													</div>
 												</div>
 											</div>
+										</div>
 										// </>
 									);
 								}}
