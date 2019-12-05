bdb3c33e76adeb9efeef95945e3d6de8ea5de6fa
Andrew Reese <ask@hyliancoder.com>
2019-12-04 19:06:48 -0600

    stuck on this maincontent display of data. stopping for now.

--- src/components/Post/MainContent.js
+++ src/components/Post/MainContent.js
@@ -4,17 +4,14 @@ import Preview from "../PostPreview";
 
 
 export default function MainContent(props) {
-  const { image, content } = props;
+  const { image, content, meta } = props;
   const rawContent = content;
 
   const fetchPublication = rawContent => {
     return fetch(`/.netlify/functions/open-graph-preview?q=${rawContent}`)
       .then(response => {
         response.json().then(json => {
-            const newContent = Object.entries(json).map(([key, value]) => [key, value]);
-            console.log(newContent)
-            console.log(newContent.meta)
-            console.log(newContent.meta.ogTitle)
+            //return json as newContent so i can do below
         })
       });
   }
@@ -24,9 +21,9 @@ export default function MainContent(props) {
 
   return (
     <div className='postContainer'>
-      <p>{rawContent}</p>
-     <Preview {...rawContent.meta} /> 
-      {!rawContent.meta && image && (
+      <p>{newContent.text}</p>
+      {newContent.meta ? <Preview {...newContent.meta} /> : null}
+      {!newContent.meta && image && (
         <div className='post-img'>
           <img className='postImage' alt='postImage' src={image} />
         </div>
