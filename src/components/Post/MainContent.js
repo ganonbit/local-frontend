f0ebf6fbca6ea85787af3aa01100f7e77c0a4035
Andrew Reese <ask@hyliancoder.com>
2019-12-04 18:59:06 -0600

    fixing filename issues

--- src/components/Post/MainContent.js
+++ src/components/Post/MainContent.js
@@ -7,21 +7,24 @@ export default function MainContent(props) {
   const { image, content } = props;
   const rawContent = content;
 
-  const fetchPublication = (rawContent) => {
+  const fetchPublication = rawContent => {
     return fetch(`/.netlify/functions/open-graph-preview?q=${rawContent}`)
       .then(response => {
-        response.json().then(parsedJson => {
-          console.log('This is the parsed json', parsedJson);
-          console.log(parsedJson.text);
+        response.json().then(json => {
+            const newContent = Object.entries(json).map(([key, value]) => [key, value]);
+            console.log(newContent)
+            console.log(newContent.meta)
+            console.log(newContent.meta.ogTitle)
         })
       });
   }
   
   fetchPublication(rawContent);
 
+
   return (
     <div className='postContainer'>
-      <p>{rawContent.text}</p>
+      <p>{rawContent}</p>
      <Preview {...rawContent.meta} /> 
       {!rawContent.meta && image && (
         <div className='post-img'>
