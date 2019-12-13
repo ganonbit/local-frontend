21a90846670817e797b9e1feedb5c44b656a7de5
Andrew Reese <ask@hyliancoder.com>
2019-12-13 05:20:40 -0600

    added reward copy

--- src/pages/Reward/RewardCard.js
+++ src/pages/Reward/RewardCard.js
@@ -2,19 +2,27 @@ import React from 'react';
 const RewardCard = () => (
   <div className='col-lg-3'>
     <div className='point-inner-wrap'>
-      <h4>20-50 Points</h4>
+      <h4>0-10 POINTS</h4>
       <ul>
         <li>
-          <span>Post</span>
-          <h6>20</h6>
+          <span>Register your account</span>
+          <h6>10</h6>
         </li>
         <li>
-          <span>Post</span>
-          <h6>20</h6>
+          <span>Watch a video from Selma</span>
+          <h6>5</h6>
         </li>
         <li>
-          <span>Comment From Refer</span>
-          <h6>20</h6>
+          <span>Rank in Top Posts and/or Top Users two weeks in a row</span>
+          <h6>10</h6>
+        </li>
+        <li>
+          <span>Comment on someone’s post</span>
+          <h6>10</h6>
+        </li>
+        <li>
+          <span>Visit the site 3 or more times per week</span>
+          <h6>5</h6>
         </li>
       </ul>
     </div>
--- src/pages/Reward/index.js
+++ src/pages/Reward/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import RewardCard from './RewardCard';
 const Reward = () => {
   return (
     <>
@@ -31,7 +30,7 @@ const Reward = () => {
             <div className='point-wrap'>
               <ul>
                 <li>your points</li>
-                <li>5,000</li>
+                <li>0</li>
               </ul>
             </div>
             <div className='point-system'>
@@ -41,16 +40,76 @@ const Reward = () => {
                 much each action is worth.
               </p>
               <div className='row point-row'>
-                <RewardCard />
-                <RewardCard />
-                <RewardCard />
-                <RewardCard />
+                <div className='col-lg-4'>
+                  <div className='point-inner-wrap'>
+                    <h4>0-10 POINTS</h4>
+                    <ul>
+                      <li>
+                        <span>Register your account</span>
+                        <h6>10</h6>
+                      </li>
+                      <li>
+                        <span>Watch a video from Selma</span>
+                        <h6>5</h6>
+                      </li>
+                      <li>
+                        <span>Rank in Top Posts and/or Top Users two weeks in a row</span>
+                        <h6>10</h6>
+                      </li>
+                      <li>
+                        <span>Comment on someone’s post</span>
+                        <h6>10</h6>
+                      </li>
+                      <li>
+                        <span>Visit the site 3 or more times per week</span>
+                        <h6>5</h6>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className='col-lg-4'>
+                  <div className='point-inner-wrap'>
+                    <h4>11-20 POINTS</h4>
+                    <ul>
+                      <li>
+                        <span>Share on social media</span>
+                        <h6>15</h6>
+                      </li>
+                      <li>
+                        <span>Your referral likes their first post</span>
+                        <h6>15</h6>
+                      </li>
+                      <li>
+                        <span>Invite your friends with the refer link</span>
+                        <h6>20</h6>
+                      </li>
+                      <li>
+                        <span>Your referral comments on their first post</span>
+                        <h6>10</h6>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className='col-lg-4'>
+                  <div className='point-inner-wrap'>
+                    <h4>21+ POINTS</h4>
+                    <ul>
+                      <li>
+                        <span>Submit an idea to Selma in the form  </span>
+                        <h6>30</h6>
+                      </li>
+                      <li>
+                        <span>Selma implements your idea</span>
+                        <h6>50</h6>
+                      </li>
+                      <li>
+                        <span>Your friend creates an account from your referral link</span>
+                        <h6>40</h6>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
               </div>
-              <h3>points system</h3>
-              <p>
-                Here is a breakdown of which actions acquires points and how
-                much each action is worth.
-              </p>
             </div>
           </div>
         </div>
