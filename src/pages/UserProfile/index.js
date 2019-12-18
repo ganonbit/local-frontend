022e4270a317316ca5241866e07ee2de8d960354
syedakhter <dev.syed.akhter@gmail.com>
2019-12-18 17:20:50 -0500

    Implemented profile and header photo, added react-google recaptcha, customized css

--- src/pages/UserProfile/index.js
+++ src/pages/UserProfile/index.js
@@ -25,7 +25,7 @@ export default function Profile(props) {
         ) : (
           <div className='container'>
             <div className='row ' style={{ paddingLeft: 0, paddingRight: 0 }}>
-              <ProfileHeader user={data.getUser} />
+              <ProfileHeader user={data.getUser} refetch={props.refetch} />
               <Left user={data.getUser}/>
               <div className='col col-xl-6 order-xl-2 col-lg-12 order-lg-2 col-md-12 order-md-1 order-sm-1 col-xs-12 order-xs-1 col-12'>
                 <div id='newsfeed-items-grid'>
