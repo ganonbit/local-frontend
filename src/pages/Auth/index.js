f3bc82fa7a3d94a39e4e38d9fe7dcd482e6ab353
Andrew Reese <ask@hyliancoder.com>
2019-12-20 03:39:22 -0600

    added reset password working again

--- src/pages/Auth/index.js
+++ src/pages/Auth/index.js
@@ -2,5 +2,5 @@ export { default as MainLayout } from './MainLayout';
 export { default as SignIn } from './SignIn';
 export { default as SignUp } from './SignUp';
 // export { default as ForgotPassword } from "./ForgotPassword";
-// export { default as ResetPassword } from "./ResetPassword";
+export { default as ResetPassword } from "./ResetPassword";
 export { default as VerifyAccount } from './VerifyAccount';
