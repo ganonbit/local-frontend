92df210f4f7746c466ce4a443a2ff38d69971879
Andrew Reese <ask@hyliancoder.com>
2019-12-20 12:30:02 -0600

    fixed netlify.toml. removed trending

--- src/pages/Welcome/RightSection/index.js
+++ src/pages/Welcome/RightSection/index.js
@@ -5,7 +5,7 @@ import TopFriends from './TopFriends';
 const RightSection = () => {
   return (
     <aside className='col col-xl-3 order-xl-3 col-lg-6 order-lg-3 col-md-6 col-sm-6 col-12 ui-block-blur'>
-      <TrendingTopics />
+      {/* <TrendingTopics /> */}
       <TopFriends />
     </aside>
   );
