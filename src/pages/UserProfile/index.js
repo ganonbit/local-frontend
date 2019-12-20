f69eb277d9d4850e1b866c4859abd0558a43ae5a
Andrew Reese <ask@hyliancoder.com>
2019-12-19 18:47:34 -0600

    removed ability to edit photos of anyone but your own on your profile

--- src/pages/UserProfile/index.js
+++ src/pages/UserProfile/index.js
@@ -6,8 +6,11 @@ import Right from './RightSidebar';
 import Post from 'components/Post';
 import { Query } from 'react-apollo';
 import { GET_USER_POSTS, GET_USER } from 'graphql/user';
+import { useStore } from 'store';
 
 export default function Profile(props) {
+  const [{ auth }] = useStore();
+  console.log()
   const queryOptions = {
     query: GET_USER_POSTS,
     variables: { username: props.match.params.username, skip: 0, limit: 15 },
@@ -25,7 +28,7 @@ export default function Profile(props) {
         ) : (
           <div className='container'>
             <div className='row ' style={{ paddingLeft: 0, paddingRight: 0 }}>
-              <ProfileHeader user={data.getUser} refetch={props.refetch} />
+              <ProfileHeader auth={auth} username={props.match.params.username} user={data.getUser} refetch={props.refetch} />
               <Left user={data.getUser} />
               <div className='col col-xl-6 order-xl-2 col-lg-12 order-lg-2 col-md-12 order-md-1 order-sm-1 col-xs-12 order-xs-1 col-12'>
                 <div id='newsfeed-items-grid'>
