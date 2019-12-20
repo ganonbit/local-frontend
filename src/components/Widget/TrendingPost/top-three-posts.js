bc55d9efb4bb9fef822e8a195e7e740462e7203e
jeremy perez <glyde06@hotmail.com>
2019-12-20 02:51:51 -0600

    Render count in top posts

--- src/components/Widget/TrendingPost/top-three-posts.js
+++ src/components/Widget/TrendingPost/top-three-posts.js
@@ -3,10 +3,11 @@ import { connectHits } from 'react-instantsearch-core'
 
 class Hits extends React.Component {
 
-  renderPostWithImage = (hit) => {
+  renderPostWithImage = (hit, count) => {
     if (!hit || !hit.image) { return null}
     return (
       <li>
+        <h5 class="text-capitalize text-success mb-2">#{count}</h5><br/>
         <h5 className="text-capitalize">{hit.author && hit.author.firstName} {hit.author && hit.author.lastName}</h5>
         <img
           className='video-bnr my-3'
@@ -19,11 +20,12 @@ class Hits extends React.Component {
     )
   }
 
-  renderPostWithoutImage = (hit) => {
-    if (!hit || !hit.content || !hit.numOfLikes) { return null}
+  renderPostWithoutImage = (hit, count) => {
+    if (!hit || !hit.content) { return null}
     return (
       <li className='inline-items' >
         <div className='notification-event'>
+          <h5 class="text-capitalize text-success">#{count}</h5>
           <h5 className="text-capitalize">{hit.author && hit.author.firstName} {hit.author && hit.author.lastName}</h5>
           <span className='notification-friend text-dark my-3'>
             {hit.content}
@@ -41,13 +43,13 @@ class Hits extends React.Component {
 
       <div className='ui-block'>
       <div className='ui-block-title ui-block-post'>
-        <h6 className='title'>Top Posts</h6>
+        <h6 className='title'>Top 3 Posts</h6>
       </div>
       <div className='ui-block-content'>
         <ul className='widget w-friend-pages-added notification-list friend-requests'>
-          {hits.slice(0, 4).map(hit => (
+          {hits.slice(0, 3).map((hit, count) => (
             <a href={`/${hit.author && hit.author.username}`}>
-              {hit.image ? this.renderPostWithImage(hit) : this.renderPostWithoutImage(hit)}
+              {hit.image ? this.renderPostWithImage(hit, count + 1) : this.renderPostWithoutImage(hit, count + 1)}
             </a>
           ))}
         </ul>
