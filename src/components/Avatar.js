399fb784aa12ad8bb88c1ed8b71ab7a26898d786
Andrew Reese <ask@hyliancoder.com>
2020-01-06 13:14:52 -0600

    cleanup of css

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Avatar = ({ size, image }) => (
   <div className="avatar-wrapper" size={size}>
     {image ? (
-      <img className="avatar-img" src={image} />
+      <img alt="avatar" className="avatar-img" src={image} />
     ) : (
-      <img className="avatar-img" src='https://res.cloudinary.com/weare270b/image/upload/v1576220262/static/Image_from_iOS_1_bnaxnc.jpg' />
+      <img alt="avatar" className="avatar-img" src='https://res.cloudinary.com/weare270b/image/upload/v1576220262/static/Image_from_iOS_1_bnaxnc.jpg' />
     )}
   </div>
 );
