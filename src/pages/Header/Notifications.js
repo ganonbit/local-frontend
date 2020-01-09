fb69e34f1fda4016518746b2250d6322002dbf6e
Andrew Krause <andrew.l.krause@gmail.com>
2020-01-08 19:59:02 -0600

    start positioning mobile searchbar under search icon

--- src/pages/Header/Notifications.js
+++ src/pages/Header/Notifications.js
@@ -24,154 +24,155 @@ const Notifications = ({ client, refetch }) => {
   };
 
   return (
-    <div className='icon-outer' onMouseLeave={() => updateNotificationSeen()}>
-      <div className='control-icon more has-items'>
-        <img
-          src='https://res.cloudinary.com/weare270b/image/upload/v1575849612/static/notification-img_cporyu.png'
-          alt='notifi'
-        />
-        {!auth.user.newNotifications.length ? null : (
-          <div className='label-avatar bg-primary'>
-            {auth.user.newNotifications.length}
-          </div>
-        )}
-        <div className='more-dropdown more-with-triangle triangle-top-center'>
-          <div className='ui-block-title ui-block-title-small'>
-            <h6 className='title'>Notifications</h6>
-            {/* <a href>Mark all as read</a>
-            <a href>Settings</a> */}
-          </div>
-
-          <div className='mCustomScrollbar' data-mcs-theme='dark'>
-            <ul className='notification-list'>
-              <li>
-                <div className='author-thumb'>
-                  <img
-                    src='https://res.cloudinary.com/weare270b/image/upload/v1576220262/static/Image_from_iOS_1_bnaxnc.jpg'
-                    alt='author'
-                  />
-                </div>
-                <div className='notification-event'>
-                  <div>
-                    <a href className='h6 notification-friend'>
-                      Reese
-                    </a>{' '}
-                    commented on your new{' '}
-                    <a href className='notification-link'>
-                      post
-                    </a>
-                  </div>
-                  <span className='notification-date'>
-                    <time
-                      className='entry-date updated'
-                      dateTime='2004-07-24T18:18'
-                    >
-                      4 hours ago
-                    </time>
-                  </span>
-                </div>
-                <span className='notification-icon'>
-                  <svg
-                    className='olymp-comments-post-icon'
-                    xlinkHref='svg-icons/sprites/icons.svg#olymp-comments-post-icon'
-                  />
-                </span>
-
-                <div className='more'>
-                  <svg
-                    className='olymp-three-dots-icon'
-                    xlinkHref='svg-icons/sprites/icons.svg#olymp-three-dots-icon'
-                  />
-                  <svg
-                    className='olymp-little-delete'
-                    xlinkHref='svg-icons/sprites/icons.svg#olymp-little-delete'
-                  />
-                </div>
-              </li>
-
-              <li className='un-read'>
-                <div className='author-thumb'>
-                  <img
-                    src='https://res.cloudinary.com/weare270b/image/upload/v1576220262/static/Image_from_iOS_1_bnaxnc.jpg'
-                    alt='author'
-                  />
-                </div>
-                <div className='notification-event'>
-                  <div>
-                    <a href className='h6 notification-friend'>
-                      Nicholas Grissom
-                    </a>{' '}
-                    just followed you!
-                  </div>
-                  <span className='notification-date'>
-                    <time
-                      className='entry-date updated'
-                      dateTime='2004-07-24T18:18'
-                    >
-                      9 hours ago
-                    </time>
-                  </span>
-                </div>
-                <span className='notification-icon'>
-                  <svg
-                    className='olymp-happy-face-icon'
-                    xlinkHref='svg-icons/sprites/icons.svg#olymp-happy-face-icon'
-                  />
-                </span>
-
-                <div className='more'>
-                  <svg
-                    className='olymp-three-dots-icon'
-                    xlinkHref='svg-icons/sprites/icons.svg#olymp-three-dots-icon'
-                  />
-                  <svg
-                    className='olymp-little-delete'
-                    xlinkHref='svg-icons/sprites/icons.svg#olymp-little-delete'
-                  />
-                </div>
-              </li>
-
-              <li className='with-comment-photo'>
-                <div className='author-thumb'>
-                  <img
-                    src='https://res.cloudinary.com/weare270b/image/upload/v1576220262/static/Image_from_iOS_1_bnaxnc.jpg'
-                    alt='author'
-                  />
-                </div>
-                <div className='notification-event'>
-                  <div>
-                    <a href className='h6 notification-friend'>
-                      Sarah Hetfield
-                    </a>{' '}
-                    commented on your{' '}
-                    <a href className='notification-link'>
-                      photo
-                    </a>
-                    .
-                  </div>
-                  <span className='notification-date'>
-                    <time
-                      className='entry-date updated'
-                      dateTime='2004-07-24T18:18'
-                    >
-                      Yesterday at 5:32am
-                    </time>
-                  </span>
-                </div>
-                <span className='notification-icon'>
-                  <svg
-                    className='olymp-comments-post-icon'
-                    xlinkHref='svg-icons/sprites/icons.svg#olymp-comments-post-icon'
-                  />
-                </span>
-              </li>
-            </ul>
-          </div>
-
-          <a href className='view-all bg-primary'>
-            View All Notifications
-          </a>
+    <div
+      className='control-icon more has-items'
+      onMouseLeave={() => updateNotificationSeen()}
+    >
+      <img
+        src='https://res.cloudinary.com/weare270b/image/upload/v1575849612/static/notification-img_cporyu.png'
+        alt='notifi'
+      />
+      {!auth.user.newNotifications.length ? null : (
+        <div className='label-avatar bg-primary'>
+          {auth.user.newNotifications.length}
         </div>
+      )}
+      <div className='more-dropdown more-with-triangle triangle-top-center'>
+        <div className='ui-block-title ui-block-title-small'>
+          <h6 className='title'>Notifications</h6>
+          {/* <a href>Mark all as read</a>
+            <a href>Settings</a> */}
+        </div>
+
+        <div className='mCustomScrollbar' data-mcs-theme='dark'>
+          <ul className='notification-list'>
+            <li>
+              <div className='author-thumb'>
+                <img
+                  src='https://res.cloudinary.com/weare270b/image/upload/v1576220262/static/Image_from_iOS_1_bnaxnc.jpg'
+                  alt='author'
+                />
+              </div>
+              <div className='notification-event'>
+                <div>
+                  <a href className='h6 notification-friend'>
+                    Reese
+                  </a>{' '}
+                  commented on your new{' '}
+                  <a href className='notification-link'>
+                    post
+                  </a>
+                </div>
+                <span className='notification-date'>
+                  <time
+                    className='entry-date updated'
+                    dateTime='2004-07-24T18:18'
+                  >
+                    4 hours ago
+                  </time>
+                </span>
+              </div>
+              <span className='notification-icon'>
+                <svg
+                  className='olymp-comments-post-icon'
+                  xlinkHref='svg-icons/sprites/icons.svg#olymp-comments-post-icon'
+                />
+              </span>
+
+              <div className='more'>
+                <svg
+                  className='olymp-three-dots-icon'
+                  xlinkHref='svg-icons/sprites/icons.svg#olymp-three-dots-icon'
+                />
+                <svg
+                  className='olymp-little-delete'
+                  xlinkHref='svg-icons/sprites/icons.svg#olymp-little-delete'
+                />
+              </div>
+            </li>
+
+            <li className='un-read'>
+              <div className='author-thumb'>
+                <img
+                  src='https://res.cloudinary.com/weare270b/image/upload/v1576220262/static/Image_from_iOS_1_bnaxnc.jpg'
+                  alt='author'
+                />
+              </div>
+              <div className='notification-event'>
+                <div>
+                  <a href className='h6 notification-friend'>
+                    Nicholas Grissom
+                  </a>{' '}
+                  just followed you!
+                </div>
+                <span className='notification-date'>
+                  <time
+                    className='entry-date updated'
+                    dateTime='2004-07-24T18:18'
+                  >
+                    9 hours ago
+                  </time>
+                </span>
+              </div>
+              <span className='notification-icon'>
+                <svg
+                  className='olymp-happy-face-icon'
+                  xlinkHref='svg-icons/sprites/icons.svg#olymp-happy-face-icon'
+                />
+              </span>
+
+              <div className='more'>
+                <svg
+                  className='olymp-three-dots-icon'
+                  xlinkHref='svg-icons/sprites/icons.svg#olymp-three-dots-icon'
+                />
+                <svg
+                  className='olymp-little-delete'
+                  xlinkHref='svg-icons/sprites/icons.svg#olymp-little-delete'
+                />
+              </div>
+            </li>
+
+            <li className='with-comment-photo'>
+              <div className='author-thumb'>
+                <img
+                  src='https://res.cloudinary.com/weare270b/image/upload/v1576220262/static/Image_from_iOS_1_bnaxnc.jpg'
+                  alt='author'
+                />
+              </div>
+              <div className='notification-event'>
+                <div>
+                  <a href className='h6 notification-friend'>
+                    Sarah Hetfield
+                  </a>{' '}
+                  commented on your{' '}
+                  <a href className='notification-link'>
+                    photo
+                  </a>
+                  .
+                </div>
+                <span className='notification-date'>
+                  <time
+                    className='entry-date updated'
+                    dateTime='2004-07-24T18:18'
+                  >
+                    Yesterday at 5:32am
+                  </time>
+                </span>
+              </div>
+              <span className='notification-icon'>
+                <svg
+                  className='olymp-comments-post-icon'
+                  xlinkHref='svg-icons/sprites/icons.svg#olymp-comments-post-icon'
+                />
+              </span>
+            </li>
+          </ul>
+        </div>
+
+        <a href className='view-all bg-primary'>
+          View All Notifications
+        </a>
       </div>
     </div>
   );
--- src/route/TopHeader.js
+++ src/route/TopHeader.js
@@ -1,27 +1,66 @@
-import React from 'react';
+import React, { useState, setState } from 'react';
 
 import '../assets/svg-icons/back-to-top.svg';
 import HeaderTitle from '../pages/Header/HeaderTitle';
 import Search from '../components/Search';
+import Default, {
+  Desktop,
+  Tablet,
+  Mobile,
+} from '../components/Wrappers/Queries';
+import { library } from '@fortawesome/fontawesome-svg-core';
+
 import { useStore } from 'store';
 import UserReferralModal from '../pages/Header/UserReferralModal';
+import Notifications from '../pages/Header/Notifications';
+import ChatNotifications from '../pages/Header/ChatNotifications';
+import AuthorPage from '../pages/Header/AuthorPage';
+
+/*
+  TODO: 
+  -- Expand search into individual components for more flexibility
+*/
 
 const TopHeader = ({ refetch }) => {
   const [{ auth }] = useStore();
 
   return (
-    <header className='header' id='site-header'>
-      <HeaderTitle />
-      <div className='header-content-wrapper'>
-        <Search refetch={refetch} auth={auth} />
-      </div>
-      <div className='nav-right-section'>
-        {/* 
-                    <Notifications />
-                    <AuthorPage /> */}
-      </div>
-      <UserReferralModal />
-    </header>
+    <>
+      <Default>
+        <header className='header' id='site-header'>
+          <HeaderTitle />
+          <div className='header-content-wrapper'>
+            <Search refetch={refetch} auth={auth} />
+
+            <div className='control-block'>
+              <ChatNotifications />
+              <Notifications refetch={refetch} />
+              <AuthorPage user={auth.user} />
+            </div>
+          </div>
+
+          <UserReferralModal />
+        </header>
+      </Default>
+      <Mobile>
+        <header
+          className='header header-responsive'
+          id='site-header-responsive'
+        >
+          <ul className='nav nav-tabs mobile-app-tabs' role='tablist'>
+            <li className='nav-item'>
+              <ChatNotifications />
+            </li>
+            <li className='nav-item'>
+              <Notifications refetch={refetch} />
+            </li>
+            <li className='nav-item'>
+              <Search refetch={refetch} auth={auth} />
+            </li>
+          </ul>
+        </header>
+      </Mobile>
+    </>
   );
 };
 export default TopHeader;
