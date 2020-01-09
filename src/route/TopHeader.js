a41523eb464d1679b9801bc0f04744003bf26e01
Andrew Krause <andrew.l.krause@gmail.com>
2020-01-08 22:35:12 -0600

    split desktop and mobile navs into their own file for easy workings

--- src/route/TopHeader.js
+++ src/route/TopHeader.js
@@ -1,72 +1,31 @@
-import React, { useState, setState } from 'react';
+import React from 'react';
+import { useMediaQuery } from 'react-responsive';
 
 import '../assets/svg-icons/back-to-top.svg';
 import HeaderTitle from '../pages/Header/HeaderTitle';
-import Search from '../components/Search';
-import Default, {
-  Desktop,
-  Tablet,
-  Mobile,
-} from '../components/Wrappers/Queries';
-import { library } from '@fortawesome/fontawesome-svg-core';
 
 import { useStore } from 'store';
 import UserReferralModal from '../pages/Header/UserReferralModal';
-import Notifications from '../pages/Header/Notifications';
-import ChatNotifications from '../pages/Header/ChatNotifications';
-import AuthorPage from '../pages/Header/AuthorPage';
 
-/*
-  TODO: 
-  -- Expand search into individual components for more flexibility
-*/
+import DesktopNav, { MobileNav } from '../pages/Header/Navigation';
 
 const TopHeader = ({ refetch }) => {
   const [{ auth }] = useStore();
+  const isMobile = useMediaQuery({ maxWidth: 767 });
 
   return (
-    <>
-      <Default>
-        <header className='header' id='site-header'>
-          <HeaderTitle />
-          <div className='header-content-wrapper'>
-            <Search refetch={refetch} auth={auth} />
+    <header
+      className={isMobile ? 'header header-responsive' : 'header'}
+      id={isMobile ? 'site-header' : 'site-header-responsive'}
+    >
+      <HeaderTitle />
+      <div className='header-content-wrapper'>
+        <DesktopNav refetch={refetch} auth={auth} />
+        <MobileNav refetch={refetch} auth={auth} />
+      </div>
 
-            <div className='control-block'>
-              <ChatNotifications />
-              <Notifications refetch={refetch} />
-              <AuthorPage user={auth.user} />
-            </div>
-          </div>
-
-          <UserReferralModal />
-        </header>
-      </Default>
-      <Mobile>
-        <header
-          className='header header-responsive'
-          id='site-header-responsive'
-        >
-          <ul className='nav nav-tabs mobile-app-tabs' role='tablist'>
-            <li className='nav-item'>
-              <div className='nav-link active'>
-                <ChatNotifications />
-              </div>
-            </li>
-            <li className='nav-item'>
-              <div className='nav-link'>
-                <Notifications refetch={refetch} />
-              </div>
-            </li>
-            <li className='nav-item'>
-              <div className='nav-link'>
-                <Search refetch={refetch} auth={auth} />
-              </div>
-            </li>
-          </ul>
-        </header>
-      </Mobile>
-    </>
+      <UserReferralModal />
+    </header>
   );
 };
 export default TopHeader;
