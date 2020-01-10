25779498343ff5a7654cc03e5e49d5c1f938e7d4
Andrew Krause <andrew.l.krause@gmail.com>
2020-01-09 20:41:29 -0600

    reworking mobile navigation with the correct tabs

--- src/pages/Header/Navigation.js
+++ src/pages/Header/Navigation.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { useMediaQuery } from 'react-responsive';
 
+import { Nav } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faBell } from '@fortawesome/free-solid-svg-icons';
+
 import Search from '../../components/Search';
 import Default, { Mobile } from '../../components/Wrappers/Queries';
-
 import Notifications from './Notifications';
 import ChatNotifications from './ChatNotifications';
 import AuthorPage from './AuthorPage';
@@ -22,29 +25,93 @@ const DesktopNav = ({ refetch, auth }) => {
       <Search refetch={refetch} auth={auth} />
       <div className='control-block'>
         <ChatNotifications />
-        <Notifications refetch={refetch} />
+        <Notifications refetch={refetch} isMobile={false} />
         <AuthorPage user={auth.user} />
       </div>
     </Default>
   );
 };
 
-export const MobileNav = ({ refetch, auth }) => {
+// REBUILD THE NAVIGATION USING BOOTSTRAP PANES / TABS
+export const MobileNav = ({ refetch, auth, mobileToggle }) => {
   return (
     <Mobile>
-      <ul className='nav nav-tabs mobile-app-tabs' role='tablist'>
-        <NavItem active={true}>
-          <ChatNotifications />
-        </NavItem>
-        <NavItem active={false}>
-          <Notifications refetch={refetch} />
-        </NavItem>
-        <NavItem active={false}>
-          <Search refetch={refetch} auth={auth} />
-        </NavItem>
-      </ul>
+      {!mobileToggle && (
+        <Nav justify variant='tabs' defaultActiveKey='/home'>
+          <Nav.Item>
+            <Nav.Link href='/home'>Active</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey='link-1'>Loooonger NavLink</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey='link-2'>Link</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey='disabled' disabled>
+              Disabled
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+      )}
+      {mobileToggle && (
+        <div className='tab-content tab-content-responsive'>
+          <Notifications refetch={refetch} isMobile={true} />
+        </div>
+      )}
     </Mobile>
   );
 };
 
 export default DesktopNav;
+
+{
+  /* <Nav justify variant="tabs" defaultActiveKey="/home">
+  <Nav.Item>
+    <Nav.Link href="/home">Active</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="link-1">Loooonger NavLink</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="link-2">Link</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="disabled" disabled>
+      Disabled
+    </Nav.Link>
+  </Nav.Item>
+</Nav> */
+}
+
+// <ul className='nav nav-tabs mobile-app-tabs' role='tablist'>
+//           <NavItem active={true}>
+//             <FontAwesomeIcon
+//               size='2x'
+//               color='white'
+//               icon={faHome}
+//               style={{ height: '24px', marginTop: '5px' }}
+//             />
+//           </NavItem>
+//           <NavItem active={false}>
+//             <ChatNotifications />
+//           </NavItem>
+//           <NavItem active={false}>
+//             <a
+//               href='#notifications'
+//               data-toggle='tab'
+//               role='tab'
+//               selected='true'
+//             >
+//               <FontAwesomeIcon
+//                 size='2x'
+//                 color='white'
+//                 icon={faBell}
+//                 style={{ height: '24px', marginTop: '5px' }}
+//               />
+//             </a>
+//           </NavItem>
+//           <NavItem active={false}>
+//             <Search refetch={refetch} auth={auth} />
+//           </NavItem>
+//         </ul>
--- src/pages/Header/Notifications.js
+++ src/pages/Header/Notifications.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { useStore } from 'store';
 import PropTypes from 'prop-types';
 import { withApollo } from 'react-apollo';
+import { useMediaQuery } from 'react-responsive';
 
 import { UPDATE_NOTIFICATION_SEEN } from 'graphql/notification';
+import Default, { Mobile } from '../../components/Wrappers/Queries';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBell } from '@fortawesome/free-solid-svg-icons';
 
-const Notifications = ({ client, refetch }) => {
+const Notifications = ({ client, refetch, isMobile }) => {
   const [{ auth }] = useStore();
+  // const isMobile = useMediaQuery({ maxWidth: 767 });
 
   const updateNotificationSeen = async () => {
     try {
@@ -28,159 +31,216 @@ const Notifications = ({ client, refetch }) => {
   };
 
   return (
-    <div
-      className='control-icon more has-items'
-      onMouseLeave={() => updateNotificationSeen()}
-    >
-      <FontAwesomeIcon
-        size='2x'
-        color='white'
-        icon={faBell}
-        style={{ height: '24px' }}
-      />
-      {!auth.user.newNotifications.length ? null : (
-        <div className='label-avatar bg-primary'>
-          {auth.user.newNotifications.length}
+    <>
+      {!isMobile ? (
+        <div
+          className='control-icon more has-items'
+          onMouseLeave={() => updateNotificationSeen()}
+        >
+          <FontAwesomeIcon
+            size='2x'
+            color='white'
+            icon={faBell}
+            style={{ height: '24px' }}
+          />
+          {!auth.user.newNotifications.length ? null : (
+            <div className='label-avatar bg-primary'>
+              {auth.user.newNotifications.length}
+            </div>
+          )}
+          <div className='more-dropdown more-with-triangle triangle-top-center'>
+            <div className='ui-block-title ui-block-title-small'>
+              <h6 className='title'>Notifications</h6>
+              {/* <a href>Mark all as read</a>
+            <a href>Settings</a> */}
+            </div>
+
+            <div className='mCustomScrollbar' data-mcs-theme='dark'>
+              <ul className='notification-list'>
+                <li>
+                  <div className='author-thumb'>
+                    <img
+                      src='https://res.cloudinary.com/weare270b/image/upload/v1576220262/static/Image_from_iOS_1_bnaxnc.jpg'
+                      alt='author'
+                    />
+                  </div>
+                  <div className='notification-event'>
+                    <div>
+                      <a href className='h6 notification-friend'>
+                        Reese
+                      </a>{' '}
+                      commented on your new{' '}
+                      <a href className='notification-link'>
+                        post
+                      </a>
+                    </div>
+                    <span className='notification-date'>
+                      <time
+                        className='entry-date updated'
+                        dateTime='2004-07-24T18:18'
+                      >
+                        4 hours ago
+                      </time>
+                    </span>
+                  </div>
+                  <span className='notification-icon'>
+                    <svg
+                      className='olymp-comments-post-icon'
+                      xlinkHref='svg-icons/sprites/icons.svg#olymp-comments-post-icon'
+                    />
+                  </span>
+
+                  <div className='more'>
+                    <svg
+                      className='olymp-three-dots-icon'
+                      xlinkHref='svg-icons/sprites/icons.svg#olymp-three-dots-icon'
+                    />
+                    <svg
+                      className='olymp-little-delete'
+                      xlinkHref='svg-icons/sprites/icons.svg#olymp-little-delete'
+                    />
+                  </div>
+                </li>
+
+                <li className='un-read'>
+                  <div className='author-thumb'>
+                    <img
+                      src='https://res.cloudinary.com/weare270b/image/upload/v1576220262/static/Image_from_iOS_1_bnaxnc.jpg'
+                      alt='author'
+                    />
+                  </div>
+                  <div className='notification-event'>
+                    <div>
+                      <a href className='h6 notification-friend'>
+                        Nicholas Grissom
+                      </a>{' '}
+                      just followed you!
+                    </div>
+                    <span className='notification-date'>
+                      <time
+                        className='entry-date updated'
+                        dateTime='2004-07-24T18:18'
+                      >
+                        9 hours ago
+                      </time>
+                    </span>
+                  </div>
+                  <span className='notification-icon'>
+                    <svg
+                      className='olymp-happy-face-icon'
+                      xlinkHref='svg-icons/sprites/icons.svg#olymp-happy-face-icon'
+                    />
+                  </span>
+
+                  <div className='more'>
+                    <svg
+                      className='olymp-three-dots-icon'
+                      xlinkHref='svg-icons/sprites/icons.svg#olymp-three-dots-icon'
+                    />
+                    <svg
+                      className='olymp-little-delete'
+                      xlinkHref='svg-icons/sprites/icons.svg#olymp-little-delete'
+                    />
+                  </div>
+                </li>
+
+                <li className='with-comment-photo'>
+                  <div className='author-thumb'>
+                    <img
+                      src='https://res.cloudinary.com/weare270b/image/upload/v1576220262/static/Image_from_iOS_1_bnaxnc.jpg'
+                      alt='author'
+                    />
+                  </div>
+                  <div className='notification-event'>
+                    <div>
+                      <a href className='h6 notification-friend'>
+                        Sarah Hetfield
+                      </a>{' '}
+                      commented on your{' '}
+                      <a href className='notification-link'>
+                        photo
+                      </a>
+                      .
+                    </div>
+                    <span className='notification-date'>
+                      <time
+                        className='entry-date updated'
+                        dateTime='2004-07-24T18:18'
+                      >
+                        Yesterday at 5:32am
+                      </time>
+                    </span>
+                  </div>
+                  <span className='notification-icon'>
+                    <svg
+                      className='olymp-comments-post-icon'
+                      xlinkHref='svg-icons/sprites/icons.svg#olymp-comments-post-icon'
+                    />
+                  </span>
+                </li>
+              </ul>
+            </div>
+
+            <a href className='view-all bg-primary'>
+              View All Notifications
+            </a>
+          </div>
+        </div>
+      ) : (
+        <div className='tab-pane' id='notification' role='tabpanel'>
+          <div
+            className='mCustomScrollbar ps ps--theme_default'
+            data-mcs-theme='dark'
+          >
+            <div className='ui-block-title ui-block-title-small'>
+              <h6 className='title'>FRIEND REQUEST</h6>
+              <a href='#'>Find Friends</a>
+              <a href='#'>Settings</a>
+            </div>
+            <ul className='notification-list friend-requests'>
+              <li>
+                <div className='notification-event'>
+                  <div>
+                    <a href className='h6 notification-friend'>
+                      Nicholas Grissom
+                    </a>{' '}
+                    just followed you!
+                  </div>
+                  <span className='notification-date'>
+                    <time
+                      className='entry-date updated'
+                      dateTime='2004-07-24T18:18'
+                    >
+                      9 hours ago
+                    </time>
+                  </span>
+                </div>
+                <span className='notification-icon'>
+                  <svg
+                    className='olymp-happy-face-icon'
+                    xlinkHref='svg-icons/sprites/icons.svg#olymp-happy-face-icon'
+                  />
+                </span>
+
+                <div className='more'>
+                  <svg
+                    className='olymp-three-dots-icon'
+                    xlinkHref='svg-icons/sprites/icons.svg#olymp-three-dots-icon'
+                  />
+                  <svg
+                    className='olymp-little-delete'
+                    xlinkHref='svg-icons/sprites/icons.svg#olymp-little-delete'
+                  />
+                </div>
+              </li>
+            </ul>
+            <a href='#' className='view-all bg-blue'>
+              Check all your Events
+            </a>
+          </div>
         </div>
       )}
-      <div className='more-dropdown more-with-triangle triangle-top-center'>
-        <div className='ui-block-title ui-block-title-small'>
-          <h6 className='title'>Notifications</h6>
-          {/* <a href>Mark all as read</a>
-            <a href>Settings</a> */}
-        </div>
-
-        <div className='mCustomScrollbar' data-mcs-theme='dark'>
-          <ul className='notification-list'>
-            <li>
-              <div className='author-thumb'>
-                <img
-                  src='https://res.cloudinary.com/weare270b/image/upload/v1576220262/static/Image_from_iOS_1_bnaxnc.jpg'
-                  alt='author'
-                />
-              </div>
-              <div className='notification-event'>
-                <div>
-                  <a href className='h6 notification-friend'>
-                    Reese
-                  </a>{' '}
-                  commented on your new{' '}
-                  <a href className='notification-link'>
-                    post
-                  </a>
-                </div>
-                <span className='notification-date'>
-                  <time
-                    className='entry-date updated'
-                    dateTime='2004-07-24T18:18'
-                  >
-                    4 hours ago
-                  </time>
-                </span>
-              </div>
-              <span className='notification-icon'>
-                <svg
-                  className='olymp-comments-post-icon'
-                  xlinkHref='svg-icons/sprites/icons.svg#olymp-comments-post-icon'
-                />
-              </span>
-
-              <div className='more'>
-                <svg
-                  className='olymp-three-dots-icon'
-                  xlinkHref='svg-icons/sprites/icons.svg#olymp-three-dots-icon'
-                />
-                <svg
-                  className='olymp-little-delete'
-                  xlinkHref='svg-icons/sprites/icons.svg#olymp-little-delete'
-                />
-              </div>
-            </li>
-
-            <li className='un-read'>
-              <div className='author-thumb'>
-                <img
-                  src='https://res.cloudinary.com/weare270b/image/upload/v1576220262/static/Image_from_iOS_1_bnaxnc.jpg'
-                  alt='author'
-                />
-              </div>
-              <div className='notification-event'>
-                <div>
-                  <a href className='h6 notification-friend'>
-                    Nicholas Grissom
-                  </a>{' '}
-                  just followed you!
-                </div>
-                <span className='notification-date'>
-                  <time
-                    className='entry-date updated'
-                    dateTime='2004-07-24T18:18'
-                  >
-                    9 hours ago
-                  </time>
-                </span>
-              </div>
-              <span className='notification-icon'>
-                <svg
-                  className='olymp-happy-face-icon'
-                  xlinkHref='svg-icons/sprites/icons.svg#olymp-happy-face-icon'
-                />
-              </span>
-
-              <div className='more'>
-                <svg
-                  className='olymp-three-dots-icon'
-                  xlinkHref='svg-icons/sprites/icons.svg#olymp-three-dots-icon'
-                />
-                <svg
-                  className='olymp-little-delete'
-                  xlinkHref='svg-icons/sprites/icons.svg#olymp-little-delete'
-                />
-              </div>
-            </li>
-
-            <li className='with-comment-photo'>
-              <div className='author-thumb'>
-                <img
-                  src='https://res.cloudinary.com/weare270b/image/upload/v1576220262/static/Image_from_iOS_1_bnaxnc.jpg'
-                  alt='author'
-                />
-              </div>
-              <div className='notification-event'>
-                <div>
-                  <a href className='h6 notification-friend'>
-                    Sarah Hetfield
-                  </a>{' '}
-                  commented on your{' '}
-                  <a href className='notification-link'>
-                    photo
-                  </a>
-                  .
-                </div>
-                <span className='notification-date'>
-                  <time
-                    className='entry-date updated'
-                    dateTime='2004-07-24T18:18'
-                  >
-                    Yesterday at 5:32am
-                  </time>
-                </span>
-              </div>
-              <span className='notification-icon'>
-                <svg
-                  className='olymp-comments-post-icon'
-                  xlinkHref='svg-icons/sprites/icons.svg#olymp-comments-post-icon'
-                />
-              </span>
-            </li>
-          </ul>
-        </div>
-
-        <a href className='view-all bg-primary'>
-          View All Notifications
-        </a>
-      </div>
-    </div>
+    </>
   );
 };
 
