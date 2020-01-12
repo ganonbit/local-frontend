8945a33ddb55351ee30c1e69a9d32811d20cfcb3
jeremy perez <glyde06@hotmail.com>
2020-01-12 02:40:29 -0600

    Track feedback form input in state and post to api for email delivery upon submission

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -4,17 +4,102 @@ import WelcomeHeader from 'pages/Welcome/WelcomeHeader';
 import { Form, Button, Col, InputGroup, Dropdown } from 'react-bootstrap';
 
 const FeedbackForm = ({ isAuth }) => {
+  const feedbackEndpoint = `${process.env.REACT_APP_API_URL}`
   const [validated, setValidated] = useState(false);
+  const [values, setValues] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    feedback: '',
+    feedbackReason: null,
+  });
 
-  const handleSubmit = event => {
+  const {
+    firstName,
+    lastName,
+    email,
+    feedback,
+    feedbackReason,
+  } = values;
+
+  const handleChange = event => {
+    event.preventDefault();
+    const { name, value } = event.target;
+
+    setValues({ ...values, [name]: value });
+  };
+
+  const handleFeedbackReasonChange = (eventKey, eventObject) => {
+    setValues({ ...values, ['feedbackReason']: eventKey });
+  };
+
+  const submitFeedbackForm = async (feedback) => {
+    const mutation = `
+      mutation {
+        submitFeedbackForm(input: {
+          firstName: "${feedback.firstName}",
+          lastName: "${feedback.lastName}",
+          email: "${feedback.email}",
+          feedback: "${feedback.feedback}",
+          feedbackReason: "${feedback.feedbackReason}"
+        }){
+          message
+        }
+      }
+    `;
+
+    try {
+      const response = await fetch(feedbackEndpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'authorization': `${process.env.REACT_APP_API_AUTH_KEY}`
+        },
+        body: JSON.stringify({ query: mutation })
+      })
+      const resp = await response.json()
+      console.log(resp.data.submitFeedbackForm.message)
+    } catch (error) {
+      console.log('There was a problem submitting your feedback: ', error.message);
+    }
+  }
+
+  const handleSubmit = async event => {
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
       event.preventDefault();
       event.stopPropagation();
+    } else {
+      await submitFeedbackForm({
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        feedback: feedback,
+        feedbackReason: feedbackReason,
+      })
     }
-
     setValidated(true);
-  };
+  }
+
+  const dropdownLabel = () => {
+    if (feedbackReason) {
+      switch (feedbackReason) {
+        case 'general':
+          return 'General Feedback'
+        case 'idea':
+          return 'Idea Submission'
+        case 'bug':
+          return 'Bug Report / Feature Request'
+        case 'report':
+          return 'Report User'
+        default:
+          return 'Select Feedback Reason'
+      }
+    } else {
+      return 'Select Feedback Reason'
+    }
+  }
+
   return (
     <>
       {!isAuth && (
@@ -45,14 +130,26 @@ const FeedbackForm = ({ isAuth }) => {
                   <Form.Row>
                     <Form.Group as={Col} md='6' controlId='validationCustom01'>
                       <Form.Label>First name</Form.Label>
-                      <Form.Control required type='text' placeholder='john' />
+                      <Form.Control
+                        required
+                        type='text'
+                        placeholder='john'
+                        name='firstName'
+                        onChange={handleChange}
+                      />
                       <Form.Control.Feedback type='invalid'>
                         Please enter first name
                       </Form.Control.Feedback>
                     </Form.Group>
                     <Form.Group as={Col} md='6' controlId='validationCustom02'>
                       <Form.Label>Last name</Form.Label>
-                      <Form.Control required type='text' placeholder='smith' />
+                      <Form.Control
+                        required
+                        type='text'
+                        placeholder='smith'
+                        name='lastName'
+                        onChange={handleChange}
+                      />
                       <Form.Control.Feedback type='invalid'>
                         Please enter last name
                       </Form.Control.Feedback>
@@ -67,8 +164,10 @@ const FeedbackForm = ({ isAuth }) => {
                         <Form.Control
                           type='email'
                           placeholder='your@email.com'
+                          name='email'
                           aria-describedby='inputGroupPrepend'
                           required
+                          onChange={handleChange}
                         />
                         <Form.Control.Feedback type='invalid'>
                           Please enter an email.
@@ -81,31 +180,28 @@ const FeedbackForm = ({ isAuth }) => {
                       controlId='validationSelection'
                     >
                       <InputGroup>
-                        <Dropdown>
+                        <Dropdown
+                          name='feedbackReason'
+                          onSelect={handleFeedbackReasonChange}
+                        >
                           <Dropdown.Toggle
                             className='btn btn-sm'
                             id='dropdown-basic'
                           >
-                            Select Feedback Reason &darr;
+                            {dropdownLabel()}
                           </Dropdown.Toggle>
 
                           <Dropdown.Menu>
-                            <Dropdown.Item eventKey='general' href='#/general'>
+                            <Dropdown.Item eventKey='general'>
                               General Feedback
                             </Dropdown.Item>
-                            <Dropdown.Item eventKey='idea' href='#/idea'>
+                            <Dropdown.Item eventKey='idea'>
                               Idea Submission
                             </Dropdown.Item>
-                            <Dropdown.Item
-                              eventKey='bug'
-                              href='#/bug-or-feature'
-                            >
+                            <Dropdown.Item eventKey='bug'>
                               Bug Report / Feature Request
                             </Dropdown.Item>
-                            <Dropdown.Item
-                              eventKey='report'
-                              href='#/report-user'
-                            >
+                            <Dropdown.Item eventKey='report'>
                               Report User
                             </Dropdown.Item>
                           </Dropdown.Menu>
@@ -119,6 +215,8 @@ const FeedbackForm = ({ isAuth }) => {
                         rows='3'
                         placeholder='your feedback here...'
                         required
+                        name='feedback'
+                        onChange={handleChange}
                       />
                       <Form.Control.Feedback type='invalid'>
                         Please provide us reason for contact.
