9684ef2f43939c73cba940add80f43abe9849253
Andrew Reese <ask@hyliancoder.com>
2020-01-15 14:17:43 -0600

    HOTFIX: hid trendingtopics for now

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,4 +2,3 @@ export { isValidEmail } from './isValidEmail';
 export { isValidUserName } from './isValidUserName';
 export { validateFormField } from './validateFormField';
 export { hashtagRegex } from './hashtagRegex';
-
