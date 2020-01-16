db271dc443d7b2df560a6f1b3c5e382766ee1ee9
syedakhter <dev.syed.akhter@gmail.com>
2020-01-16 16:38:19 -0500

    ui changes on profile page

--- src/pages/UserProfile/RightSidebar/index.js
+++ src/pages/UserProfile/RightSidebar/index.js
@@ -6,11 +6,13 @@ import ProfileBadges from './ProfileBadges';
 
 export default function Right() {
   return (
-    <div className='col col-xl-3 order-xl-3 col-lg-6 order-lg-3 col-md-6 col-sm-6 order-sm-3 col-xs-12 order-xs-3 col-12'>
-      <ProfileBadges />
-      {/* <Friends /> */}
-      <LastPhotos />
-      {/* <Favs /> */}
+    <div className='col col-xl-3 order-xl-3 col-lg-6 order-lg-3 col-md-6 col-sm-6 col-12'>
+      <div className="sticky-block">
+        <ProfileBadges />
+        {/* <Friends /> */}
+        <LastPhotos />
+        {/* <Favs /> */}
+      </div>
     </div>
   );
 }
