7d24155f2924ca3560cca0dee2cc91f97705b72b
Andrew Reese <ask@hyliancoder.com>
2020-01-18 23:02:30 -0600

    removed positioning of sidebar that was breaking mobile UX

--- src/components/Sidebar/SidebarConfig.js
+++ src/components/Sidebar/SidebarConfig.js
@@ -1,48 +1,22 @@
 const Sidebar = {
-  root: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    overflow: 'hidden',
-  },
-  sidebar: {
-    position: 'fixed',
-    top: 0,
-    bottom: 0,
-    transition: 'transform .3s ease-out',
-    WebkitTransition: '-webkit-transform .3s ease-out',
-    willChange: 'transform',
-    overflowY: 'initial',
-  },
-  content: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    overflowY: 'auto',
-    WebkitOverflowScrolling: 'touch',
-    transition: 'left .3s ease-out, right .3s ease-out',
-  },
-  overlay: {
-    zIndex: 1,
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    opacity: 0,
-    visibility: 'hidden',
-    transition: 'opacity .3s ease-out, visibility .3s ease-out',
-    backgroundColor: 'rgba(0,0,0,.3)',
-  },
-  dragHandle: {
-    zIndex: 1,
-    position: 'fixed',
-    top: 0,
-    bottom: 0,
-  },
-};
-export default Sidebar;
+    sidebar: {
+      transition: 'transform .3s ease-out',
+      WebkitTransition: '-webkit-transform .3s ease-out',
+      willChange: 'transform',
+      overflowY: 'initial'
+    },
+    content: {
+      overflowY: 'auto',
+      WebkitOverflowScrolling: 'touch',
+      transition: 'left .3s ease-out, right .3s ease-out'
+    },
+    overlay: {
+      zIndex: 1,
+      opacity: 0,
+      visibility: 'hidden',
+      transition: 'opacity .3s ease-out, visibility .3s ease-out',
+      backgroundColor: 'rgba(0,0,0,.3)'
+    },
+  }
+  export default Sidebar
+  
