100e31454089df2bde96a101eac82fb411714450
Andrew Reese <ask@hyliancoder.com>
2020-01-30 18:53:27 -0600

    trying to get caching better on front end, did a handdful of improvements, but most of it needs to be done server side

--- src/utils/apollo-client.js
+++ src/utils/apollo-client.js
@@ -2,17 +2,25 @@ import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { onError } from 'apollo-link-error';
 import { ApolloLink, Observable, split } from 'apollo-link';
+import { createPersistedQueryLink } from 'apollo-link-persisted-queries';
+import { BatchHttpLink } from 'apollo-link-batch-http';
 import { createUploadLink } from 'apollo-upload-client';
 import { getMainDefinition } from 'apollo-utilities';
 import { WebSocketLink } from 'apollo-link-ws';
+import { SubscriptionClient } from 'subscriptions-transport-ws';
+import { withClientState } from 'apollo-link-state';
 
 /**
  * Creates a Apollo Link, that adds authentication token to request
  */
 const createAuthLink = () => {
-  const request = operation => {
+  const request = async (operation) => {
     const token = localStorage.getItem('token');
     operation.setContext({
+      http: {
+        includeExtensions: true,
+        includeQuery: false
+      },
       headers: {
         authorization: token,
       },
@@ -20,24 +28,23 @@ const createAuthLink = () => {
   };
 
   return new ApolloLink(
-    (operation, forward) =>
-      new Observable(observer => {
-        let handle;
-        Promise.resolve(operation)
-          .then(oper => request(oper))
-          .then(() => {
-            handle = forward(operation).subscribe({
-              next: observer.next.bind(observer),
-              error: observer.error.bind(observer),
-              complete: observer.complete.bind(observer),
-            });
-          })
-          .catch(observer.error.bind(observer));
+    (operation, forward) => new Observable((observer) => {
+      let handle;
+      Promise.resolve(operation)
+        .then(oper => request(oper))
+        .then(() => {
+          handle = forward(operation).subscribe({
+            next: observer.next.bind(observer),
+            error: observer.error.bind(observer),
+            complete: observer.complete.bind(observer)
+          });
+        })
+        .catch(observer.error.bind(observer));
 
-        return () => {
-          if (handle) handle.unsubscribe();
-        };
-      })
+      return () => {
+        if (handle) handle.unsubscribe();
+      };
+    })
   );
 };
 
@@ -47,10 +54,10 @@ const createAuthLink = () => {
 const handleErrors = () => {
   return onError(({ graphQLErrors, networkError }) => {
     if (graphQLErrors) {
-      console.log('graphQLErrors', graphQLErrors);
+      console.error({ graphQLErrors });
     }
     if (networkError) {
-      console.log('networkError', networkError);
+      console.error({ networkError});
     }
   });
 };
@@ -65,21 +72,32 @@ const handleErrors = () => {
 export const createApolloClient = (apiUrl, websocketApiUrl) => {
   const cache = new InMemoryCache();
   const errorLink = handleErrors();
-  const authLink = createAuthLink();
-  const uploadLink = createUploadLink({ uri: apiUrl }); // Upload link also creates an HTTP link
 
-  // Create WebSocket link
-  const authToken = localStorage.getItem('token');
-  const wsLink = new WebSocketLink({
+  const GRAPHQL_ENDPOINT = 'ws://localhost:4000/graphql';
+
+  const client = new SubscriptionClient(GRAPHQL_ENDPOINT, {
     uri: websocketApiUrl,
     options: {
-      timeout: 60000,
       reconnect: true,
+      timeout: 60000,
       connectionParams: {
         authorization: authToken,
       },
     },
   });
+
+  const authLink = createAuthLink();
+  const httpLink = new BatchHttpLink({
+    uri: GRAPHQL_ENDPOINT
+  });
+  const uploadLink = createUploadLink({ uri: apiUrl }); // Upload link also creates an HTTP link
+
+  // Create WebSocket link
+  const authToken = localStorage.getItem('token');
+  const wsLink = process.browser
+  ? new WebSocketLink(client): () => {
+    console.log('SSR');
+  };
 
   // Temporary fix for early websocket closure resulting in websocket connections not being instantiated
   // https://github.com/apollographql/subscriptions-transport-ws/issues/377
@@ -92,14 +110,40 @@ export const createApolloClient = (apiUrl, websocketApiUrl) => {
   const terminatingLink = split(
     ({ query }) => {
       const { kind, operation } = getMainDefinition(query);
-      return kind === 'OperationDefinition' && operation === 'subscription';
+      return (
+        kind === 'OperationDefinition'
+        && operation === 'subscription'
+        && process.browser
+      );
     },
     wsLink,
-    uploadLink
+    uploadLink,
+    httpLink
   );
 
+  const link = ApolloLink.from([
+    errorLink, 
+    authLink, 
+    withClientState({
+      defaults: {
+        isConnected: true
+      },
+      resolvers: {
+        Mutation: {
+          updateNetworkStatus: (_, { isConnected }, { cache }) => {
+            cache.writeData({ data: { isConnected } });
+            return null;
+          }
+        }
+      },
+      cache
+    }),
+    createPersistedQueryLink().concat(terminatingLink)
+  ]);
+  
+
   return new ApolloClient({
-    link: ApolloLink.from([errorLink, authLink, terminatingLink]),
-    cache,
+    link: link,
+    cache: cache,
   });
 };
