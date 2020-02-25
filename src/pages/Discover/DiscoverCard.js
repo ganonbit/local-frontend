7ce01956c13303e8ee9f695b6b3733c557ed6298
syedakhter <dev.syed.akhter@gmail.com>
2020-02-25 15:12:51 -0500

    Modified discover page/fixed first time images loaded issue

--- src/pages/Discover/DiscoverCard.js
+++ src/pages/Discover/DiscoverCard.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 
-const DiscoverCard = ({ image, description, onClickHandler }) => {
+const DiscoverCard = ({ image, description, onClickHandler, index }) => {
   return (
     <div
       className='col-md-4 p-1'
-      onClick={() => onClickHandler(image, description)}
+      tabIndex='0'
+      role='button'
+      onKeyDown={() => onClickHandler(index)}
+      onClick={() => onClickHandler(index)}
     >
       <div className='grid-caption-wrap'>
         <div className='grid-caption'>
@@ -20,4 +24,10 @@ const DiscoverCard = ({ image, description, onClickHandler }) => {
   );
 };
 
+DiscoverCard.propTypes = {
+  image: PropTypes.string.isRequired,
+  description: PropTypes.any,
+  onClickHandler: PropTypes.func.isRequired,
+  index: PropTypes.number.isRequired,
+};
 export default DiscoverCard;
--- src/pages/Discover/index.js
+++ src/pages/Discover/index.js
@@ -8,49 +8,57 @@ import DiscoverCard from './DiscoverCard';
 import DiscoverModal from './DiscoverModal';
 const masonryOptions = {
   transitionDuration: 1000,
+  // resize: true,
+  // initLayout: true,
 };
 
 const Discover = () => {
   const [isShowing, setIsShowing] = useState(false);
-  const [image, setImage] = useState(null);
-  const [description, setDescription] = useState(null);
-
+  const [isIndex, setIndex] = useState(0);
   const { loading, data } = useQuery(GET_USER_POSTS, {
     variables: { username: 'selma', skip: 0, limit: 25 },
   });
-
-  let onClickHandler = (image, description) => {
+  let onClickHandler = index => {
     setIsShowing(true);
-    setImage(image);
-    setDescription(description);
+    setIndex(index);
   };
   return (
     <>
-      <DiscoverModal
-        show={isShowing}
-        onHide={setIsShowing}
-        image={image}
-        description={description}
-      />
       <div className='container-fluid px-4'>
         {!loading && (
           <Masonry
             className={'row my-2'}
+            elementType={'div'}
             options={masonryOptions}
             disableImagesLoaded={false}
             updateOnEachImageLoad={false}
+            enableResizableChildren={true}
           >
             {data &&
               data.getUserPosts.posts.map((post, index) => {
                 return (
                   post.image && (
-                    <DiscoverCard
-                      image={post.image}
-                      description={
-                        post.content ? post.content : 'Avocado Nation'
-                      }
-                      onClickHandler={onClickHandler}
-                    />
+                    <>
+                      {isIndex === index && isShowing && (
+                        <DiscoverModal
+                          show={isShowing}
+                          onHide={setIsShowing}
+                          image={post.image}
+                          description={post.content ? post.content : ''}
+                          bio={post.author.bio}
+                          post={post}
+                        />
+                      )}
+                      <DiscoverCard
+                        image={post.image}
+                        description={post.content ? post.content : ''}
+                        bio={post.author.bio}
+                        onClickHandler={onClickHandler}
+                        post={post}
+                        index={index}
+                        key={index}
+                      />
+                    </>
                   )
                 );
               })}
